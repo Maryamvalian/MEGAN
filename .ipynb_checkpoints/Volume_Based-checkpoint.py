2a455566c1d533f15437fa7086bf853720a99c10
Maryam Valian <maryam.valian@gmail.com>
2025-04-23 15:47:12 -0400

    ed

---
+++ .ipynb_checkpoints/Volume_Based-checkpoint.py
@@ -0,0 +1,159 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+from pathlib import Path
+from eelbrain import *
+from matplotlib import pyplot
+import mne
+import numpy
+import re
+
+#Eelbrain Pipeline Setup
+# %run experiment.py           
+
+# %%
+inv_info = 'vec-3-dSPM-0'      #vector, or snr3, dspm, 0: superficial dipoles
+src_info = 'vol-10' # set up at vol-10 for testing, set to 5 for better result: Distance between the two adjacent dipoles
+e.set_inv(ori=re.split('-',inv_info)[0], 
+                snr=int(re.split('-',inv_info)[1]), 
+                method=re.split('-',inv_info)[2], 
+                depth=int(re.split('-',inv_info)[3]), 
+                pick_normal=False, 
+                src=src_info)
+e.set(parc = 'aparc+aseg')
+
+#Annotation
+wholebrain_cortical_subcortical_mask = [
+ 'Left-Cerebral-White-Matter',
+ 'Left-Lateral-Ventricle',
+ 'Left-Inf-Lat-Vent',
+ 'Left-Putamen',
+ 'Left-Hippocampus',
+ 'Left-Amygdala',
+ 'Left-Accumbens-area',
+ 'Left-vessel',
+ 'Right-Cerebral-White-Matter',
+ 'Right-Inf-Lat-Vent',
+ 'Right-Putamen',
+ 'Right-Hippocampus',
+ 'Right-Amygdala',
+ 'ctx-lh-bankssts',
+ 'ctx-lh-caudalanteriorcingulate',
+ 'ctx-lh-caudalmiddlefrontal',
+ 'ctx-lh-cuneus',
+ 'ctx-lh-entorhinal',
+ 'ctx-lh-fusiform',
+ 'ctx-lh-inferiorparietal',
+ 'ctx-lh-inferiortemporal',
+ 'ctx-lh-isthmuscingulate',
+ 'ctx-lh-lateraloccipital',
+ 'ctx-lh-lateralorbitofrontal',
+ 'ctx-lh-lingual',
+ 'ctx-lh-medialorbitofrontal',
+ 'ctx-lh-middletemporal',
+ 'ctx-lh-parahippocampal',
+ 'ctx-lh-paracentral',
+ 'ctx-lh-parsopercularis',
+ 'ctx-lh-parsorbitalis',
+ 'ctx-lh-parstriangularis',
+ 'ctx-lh-pericalcarine',
+ 'ctx-lh-postcentral',
+ 'ctx-lh-posteriorcingulate',
+ 'ctx-lh-precentral',
+ 'ctx-lh-precuneus',
+ 'ctx-lh-rostralanteriorcingulate',
+ 'ctx-lh-rostralmiddlefrontal',
+ 'ctx-lh-superiorfrontal',
+ 'ctx-lh-superiorparietal',
+ 'ctx-lh-superiortemporal',
+ 'ctx-lh-supramarginal',
+ 'ctx-lh-frontalpole',
+ 'ctx-lh-temporalpole',
+ 'ctx-lh-transversetemporal',
+ 'ctx-lh-insula',
+ 'ctx-rh-bankssts',
+ 'ctx-rh-caudalanteriorcingulate',
+ 'ctx-rh-caudalmiddlefrontal',
+ 'ctx-rh-cuneus',
+ 'ctx-rh-entorhinal',
+ 'ctx-rh-fusiform',
+ 'ctx-rh-inferiorparietal',
+ 'ctx-rh-inferiortemporal',
+ 'ctx-rh-isthmuscingulate',
+ 'ctx-rh-lateraloccipital',
+ 'ctx-rh-lateralorbitofrontal',
+ 'ctx-rh-lingual',
+ 'ctx-rh-medialorbitofrontal',
+ 'ctx-rh-middletemporal',
+ 'ctx-rh-parahippocampal',
+ 'ctx-rh-paracentral',
+ 'ctx-rh-parsopercularis',
+ 'ctx-rh-parsorbitalis',
+ 'ctx-rh-parstriangularis',
+ 'ctx-rh-pericalcarine',
+ 'ctx-rh-postcentral',
+ 'ctx-rh-posteriorcingulate',
+ 'ctx-rh-precentral',
+ 'ctx-rh-precuneus',
+ 'ctx-rh-rostralanteriorcingulate',
+ 'ctx-rh-rostralmiddlefrontal',
+ 'ctx-rh-superiorfrontal',
+ 'ctx-rh-superiorparietal',
+ 'ctx-rh-superiortemporal',
+ 'ctx-rh-supramarginal',
+ 'ctx-rh-frontalpole',
+ 'ctx-rh-temporalpole',
+ 'ctx-rh-transversetemporal',
+ 'ctx-rh-insula']
+
+
+
+
+#check experiment e
+# %%
+e.sessions
+
+# %%
+sessions = ['word']
+e.epochs
+
+# %%
+epochs=['word-real','word-stem','word-inflected']
+group='young'
+
+# %%
+e.show_subjects()
+
+# %%
+
+results={}
+for epoch in epochs:
+    for session in sessions:
+        e.set(raw='n-ica', epoch=epoch, rej='')
+        
+        result=e.load_evoked_stc(
+            subjects=group, 
+            baseline=False,
+            cov='emptyroom', 
+            model='word',
+           )
+        results[f'{epoch}_{session}'] = result
+ 
+
+
+# %%
+
+# %%
---
+++ experiment.py
@@ -0,0 +1,235 @@
+# +
+
+"""
+Empty room
+----------
+Code for generating empty room data:
+
+    from pathlib import Path
+
+    root = Path('~/Data/root/meg')
+    src = root / 'ERT_10.16.12_9h25m-raw.fif'
+    for sdir in root.glob('R????'):
+        dst = sdir / f'{sdir.name}_emptyroom-raw.fif'
+        src.link_to(dst)
+
+
+Visits
+------
+Visit 2: pre-treatment to establish stability (2 patients missing)
+Visit 3: post-treatment
+
+
+Preprocessing
+-------------
+After taking over I additionally auto-rejected channels based on low neighbor-correlation
+
+auto-reject epochs with absolute thershold 2e-12
+- confirmed done for young
+
+    e.set(raw='n-ica-14', epoch='word', group='good')
+    for s in e:
+        e.make_epoch_selection(auto=2e-12)
+    e.set(visit='3', group='aphasia')
+    for s in e:
+        e.make_epoch_selection(auto=2e-12)
+
+
+"""
+from itertools import chain
+from pathlib import Path
+import warnings
+# -
+
+from eelbrain import *
+from eelbrain.pipeline import *
+
+# hide unimportant
+warnings.filterwarnings('ignore', category=FutureWarning)
+
+
+# Default since mne 0.16
+FILTER_KWARGS = {
+    'filter_length': 'auto',
+    'l_trans_bandwidth': 'auto',
+    'h_trans_bandwidth': 'auto',
+    'phase': 'zero',
+    'fir_window': 'hamming',
+    'fir_design': 'firwin',
+}
+
+YOUNG = ('R0240', 'R0284', 'R0645', 'R0654', 'R0656', 'R0727', 'R0728', 'R0729', 'R0730', 'R0734', 'R0773', 'R0793', 'R0885', 'R0918')
+limited_YOUNG = ('R0240', 'R0284','R0645','R0654')
+OLD = ('R0735', 'R0742', 'R0744', 'R0783', 'R0784', 'R0786', 'R0840', 'R0850', 'R0886')
+APHASIA = ('R0655', 'R0721', 'R0782', 'R0952', 'R0944', 'R1015')
+
+IFG = ('parsorbitalis', 'parstriangularis', 'parsopercularis')
+TEMPORAL = ('transversetemporal', 'superiortemporal', 'bankssts', 'middletemporal', 'inferiortemporal', 'temporalpole', 'fusiform', 'parahippocampal', 'entorhinal')
+OCCIPITAL = ('cuneus', 'pericalcarine', 'lingual', 'lateraloccipital')
+
+#ROOT = Path('dataset').expanduser()
+ROOT = Path('~/Data').expanduser()
+
+
+class Experiment(MneExperiment):
+
+    path_version = 1
+    auto_delete_cache = 'debug'
+    check_raw_mtime = False
+
+    visits = ('', '2', '3')
+
+    sessions = ('words', 'sentences', 'emptyroom')
+
+    defaults = {
+        'raw': 'n-ica',
+        #Mary 
+        'group': 'young'
+    }
+
+    raw = {
+        'tsss': RawMaxwell('raw', st_duration=10., ignore_ref=True, st_correlation=.9, st_only=True),
+        # original pipeline !! ICA without filtering
+        '1-40': RawFilter('tsss', 1, 40, **FILTER_KWARGS, cache=False),
+        'ica': RawICA('tsss', ('words', 'sentences'), n_components=0.99, random_state=0),
+        'ica1-40': RawFilter('ica', 1, 40, **FILTER_KWARGS, cache=False),
+        'ica1-20': RawFilter('ica', 1, 20, **FILTER_KWARGS, cache=False),
+        # secondary ICA
+        'n-1-20': RawFilter('tsss', 1, 20),
+        'n-ica': RawICA('n-1-20', ('words', 'sentences'), random_state=0),
+        'n-unfiltered': RawApplyICA('tsss', 'n-ica'),
+        'n-ica-14': RawFilter('n-ica', 0, 14),
+        'n-picard': RawICA('n-1-20', ('words', 'sentences'), 'picard', random_state=0, fit_params=dict(ortho=False, extended=True)),
+        # OTP
+        'filt-1-80': RawFilter('tsss', 1, 80, cache=False),
+        '1-80': RawApplyICA('filt-1-80', 'n-ica'),
+    }
+
+    def fix_events(self, ds):
+        if ds.info['session'] == 'emptyroom':
+            return ds
+        if ds[1, 'trigger'] <= 161:
+            ds['trigger'] -= 161
+            ds = ds.sub("trigger > 160")
+        elif ds.info['subject'] == 'R0782':
+            ds = ds.sub("trigger <= 165")
+            ds = ds.sub("trigger > 161")
+        return ds
+
+    def label_events(self, ds):
+        if ds.info['session'] == 'words':
+            ds['word'] = Factor(ds['trigger'], labels={162: 'real', 163: 'pseudo', 164: 'pseudo', 165: 'real'})
+            ds['inflected'] = Factor(ds['trigger'], labels={162: 'inflected', 163: 'inflected', 164: 'uninflected', 165: 'uninflected'})
+            ds['lexical'] = Factor(ds['trigger'], labels={162: 'realinflected', 163: 'pseudoinflected', 164: 'pseudouninflected', 165: 'realuninflected'})
+        elif ds.info['session'] == 'sentences':
+            ds['felicity'] = Factor(ds['trigger'], labels={162: 'bad', 163: 'good', 164: 'bad', 165: 'bad'})
+            ds['violation'] = Factor(ds['trigger'], labels={162: 'tense', 163: 'none', 164: 'semantic', 165: 'grammatical'})
+        elif ds.info['session'] != 'emptyroom':
+            raise RuntimeError(f"unknown session {ds.info['session']}")
+        return ds
+
+    epochs = {
+        'word': PrimaryEpoch('words', tmax=0.8, samplingrate=250),
+        'word-real': SecondaryEpoch('word', "word == 'real'"),
+        'word-stem': SecondaryEpoch('word', "inflected == 'uninflected'"),
+        'word-inflected': SecondaryEpoch('word', "inflected == 'inflected'"),
+        'cov': SecondaryEpoch('word', tmax=0),
+        'sentence': PrimaryEpoch('sentences', tmax=1.0, samplingrate=250),
+        'sentence-shared': SecondaryEpoch('sentence', "violation != 'grammatical'"),
+        'sentence-none': SecondaryEpoch('sentence', "violation == 'none'"),
+        'sentence-semantic': SecondaryEpoch('sentence', "violation == 'semantic'"),
+    }
+
+    groups = {
+        'bad': ['R0637'],
+        'good': SubGroup('all', ['R0637']),
+        'young': YOUNG,
+        'old': OLD,
+        'controls': YOUNG + OLD,
+        'aphasia': APHASIA,
+        'controls165': ('R0727', 'R0728', 'R0729', 'R0730', 'R0734', 'R0735', 'R0742', 'R0744', 'R0773', 'R0783', 'R0784', 'R0786', 'R0793', 'R0840', 'R0850', 'R0885', 'R0886', 'R0918'),
+        'controlsno165': ('R0240', 'R0284', 'R0645', 'R0654', 'R0656'),
+        'visit2': ('R0655', 'R0782', 'R0952', 'R1015'),
+
+        'small': limited_YOUNG,
+    }
+    
+    variables = {
+        'group': GroupVar(['young', 'old', 'aphasia', 'bad']),
+    }
+
+    parcs = {
+        'fusiform-lh': SubParc('aparc', ['fusiform-lh']),
+        'fusiform-rh': SubParc('aparc', ['fusiform-rh']),
+        'ifg-lh': SubParc('aparc', [f'{label}-lh' for label in IFG]),
+        'ifg-rh': SubParc('aparc', [f'{label}-rh' for label in IFG]),
+
+        'ot': SubParc('aparc', list(chain(TEMPORAL, OCCIPITAL)), 'lateral'),
+        'ot-lh': SubParc('aparc', [f'{label}-lh' for label in chain(TEMPORAL, OCCIPITAL)], 'lateral'),
+
+
+        'frontotemporal-lh': CombinationParc('aparc', {'frontal-lh': 'superiorfrontal + rostralmiddlefrontal + caudalmiddlefrontal+ parsorbitalis + parstriangularis + parsopercularis + lateralorbitofrontal + medialorbitofrontal + precentral + frontalpole + paracentral', 'temporal-lh': 'transversetemporal + superiortemporal + middletemporal + inferiortemporal + bankssts', }, views='lateral'),
+        'frontotemporal-rh': CombinationParc('aparc', {'frontal-rh': 'superiorfrontal + rostralmiddlefrontal + caudalmiddlefrontal+ parsorbitalis + parstriangularis + parsopercularis + lateralorbitofrontal + medialorbitofrontal + precentral + frontalpole + paracentral', 'temporal-rh': 'transversetemporal + superiortemporal + middletemporal + inferiortemporal + bankssts', }, views='lateral'),
+
+        'temporal-rh': CombinationParc('aparc', {'temporal-rh': 'transversetemporal + superiortemporal + middletemporal + inferiortemporal + bankssts', }, views='lateral'),
+        'temporal-lh': CombinationParc('aparc', {'temporal-lh': 'transversetemporal + superiortemporal + middletemporal + inferiortemporal + bankssts', }, views='lateral'),
+        'temporal2-lh': CombinationParc('aparc', {'superiortemporal-lh': 'transversetemporal + superiortemporal + bankssts', 'middletemporal-lh': 'middletemporal', 'inferiortemporal-lh': 'inferiortemporal', }, views='lateral'),
+
+        'frontal2-lh': CombinationParc('aparc', {'inferiorfrontal-lh': 'superiorfrontal + rostralmiddlefrontal', 'superiorfrontal-lh': 'caudalmiddlefrontal + parsorbitalis + parstriangularis'}),
+        'frontal-rh': CombinationParc('aparc', {'frontal-rh': 'superiorfrontal+ rostralmiddlefrontal+ caudalmiddlefrontal+ parsorbitalis + parstriangularis + parsopercularis + lateralorbitofrontal + medialorbitofrontal + precentral + frontalpole + paracentral'}),
+        'frontal-lh': CombinationParc('aparc', {'frontal-lh': 'superiorfrontal+ rostralmiddlefrontal+ caudalmiddlefrontal+ parsorbitalis + parstriangularis + parsopercularis + lateralorbitofrontal + medialorbitofrontal + precentral + frontalpole + paracentral'}),
+        'inferiorfrontal-lh': CombinationParc('aparc', {'inferiorfrontal-lh': 'parsorbitalis + parstriangularis + parsopercularis'}),
+
+        'middletemporal-lh': CombinationParc('aparc', {'middletemporal': 'middletemporal'}),
+    }
+
+    tests = {
+        '0': TTestOneSample(),
+
+        # lexical decision
+        ###################
+        'inflection': TTestRelated('inflected', 'inflected', 'uninflected'),
+        'lexical': TTestRelated('word', 'real', 'pseudo'),
+
+        # early visual
+        'early': TTestRelated('inflected', 'inflected', 'uninflected', tail=1),
+
+        # ANOVAS
+        'pseudoxinflection': ANOVA('inflected * word * subject'),
+        'lexicalxsubject': ANOVA('lexical * subject'),
+
+
+        # sentence judgment
+        ####################
+        # Violations
+        'correct': TTestRelated('grammaticality', 'correct', 'incorrect'),
+        'tense': TTestRelated('violation', 'tense', 'none'),
+        'semantic': TTestRelated('violation', 'semantic', 'none'),
+        'tense=semantic': TTestRelated('violation', 'tense', 'semantic'),
+
+        # ANOVA
+        'violation': ANOVA('violation * subject'),
+
+
+        # group differences
+        ####################
+        # group difference TBD
+        'age': TTestIndependent('group', 'old', 'young'),
+        'aphasia-controls': TTestIndependent('group', 'aphasia', 'controls'),
+        'aphasia-old': TTestIndependent('group', 'aphasia', 'old'),
+    }
+
+
+e = Experiment(ROOT)
+
+
+
+
+
+
+
+
+
+
+
+
