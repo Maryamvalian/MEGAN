4ad5b39d50ce8562dbe4240ef79f86afe8ccb331
Maryam Valian <maryam.valian@gmail.com>
2025-04-30 17:17:34 -0400

    ed

--- Volume_Based .py
+++ Volume_Based .py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.17.0
+#       jupytext_version: 1.17.1
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -25,135 +25,18 @@ import re
 # %run experiment.py           #e is the experiment object
 
 # %%
-inv_info = 'vec-3-dSPM-0'      #vector, or snr3, dspm, 0: superficial dipoles
-src_info = 'vol-10' # set up at vol-10 for testing, set to 5 for better result: Distance between the two adjacent dipoles
-e.set_inv(ori=re.split('-',inv_info)[0], 
-                snr=int(re.split('-',inv_info)[1]), 
-                method=re.split('-',inv_info)[2], 
-                depth=int(re.split('-',inv_info)[3]), 
-                pick_normal=False, 
-                src=src_info)
-e.set(parc = 'aparc+aseg')
-
-wholebrain_cortical_subcortical_mask = [
- 'Left-Cerebral-White-Matter',
- 'Left-Lateral-Ventricle',
- 'Left-Inf-Lat-Vent',
- 'Left-Putamen',
- 'Left-Hippocampus',
- 'Left-Amygdala',
- 'Left-Accumbens-area',
- 'Left-vessel',
- 'Right-Cerebral-White-Matter',
- 'Right-Inf-Lat-Vent',
- 'Right-Putamen',
- 'Right-Hippocampus',
- 'Right-Amygdala',
- 'ctx-lh-bankssts',
- 'ctx-lh-caudalanteriorcingulate',
- 'ctx-lh-caudalmiddlefrontal',
- 'ctx-lh-cuneus',
- 'ctx-lh-entorhinal',
- 'ctx-lh-fusiform',
- 'ctx-lh-inferiorparietal',
- 'ctx-lh-inferiortemporal',
- 'ctx-lh-isthmuscingulate',
- 'ctx-lh-lateraloccipital',
- 'ctx-lh-lateralorbitofrontal',
- 'ctx-lh-lingual',
- 'ctx-lh-medialorbitofrontal',
- 'ctx-lh-middletemporal',
- 'ctx-lh-parahippocampal',
- 'ctx-lh-paracentral',
- 'ctx-lh-parsopercularis',
- 'ctx-lh-parsorbitalis',
- 'ctx-lh-parstriangularis',
- 'ctx-lh-pericalcarine',
- 'ctx-lh-postcentral',
- 'ctx-lh-posteriorcingulate',
- 'ctx-lh-precentral',
- 'ctx-lh-precuneus',
- 'ctx-lh-rostralanteriorcingulate',
- 'ctx-lh-rostralmiddlefrontal',
- 'ctx-lh-superiorfrontal',
- 'ctx-lh-superiorparietal',
- 'ctx-lh-superiortemporal',
- 'ctx-lh-supramarginal',
- 'ctx-lh-frontalpole',
- 'ctx-lh-temporalpole',
- 'ctx-lh-transversetemporal',
- 'ctx-lh-insula',
- 'ctx-rh-bankssts',
- 'ctx-rh-caudalanteriorcingulate',
- 'ctx-rh-caudalmiddlefrontal',
- 'ctx-rh-cuneus',
- 'ctx-rh-entorhinal',
- 'ctx-rh-fusiform',
- 'ctx-rh-inferiorparietal',
- 'ctx-rh-inferiortemporal',
- 'ctx-rh-isthmuscingulate',
- 'ctx-rh-lateraloccipital',
- 'ctx-rh-lateralorbitofrontal',
- 'ctx-rh-lingual',
- 'ctx-rh-medialorbitofrontal',
- 'ctx-rh-middletemporal',
- 'ctx-rh-parahippocampal',
- 'ctx-rh-paracentral',
- 'ctx-rh-parsopercularis',
- 'ctx-rh-parsorbitalis',
- 'ctx-rh-parstriangularis',
- 'ctx-rh-pericalcarine',
- 'ctx-rh-postcentral',
- 'ctx-rh-posteriorcingulate',
- 'ctx-rh-precentral',
- 'ctx-rh-precuneus',
- 'ctx-rh-rostralanteriorcingulate',
- 'ctx-rh-rostralmiddlefrontal',
- 'ctx-rh-superiorfrontal',
- 'ctx-rh-superiorparietal',
- 'ctx-rh-superiortemporal',
- 'ctx-rh-supramarginal',
- 'ctx-rh-frontalpole',
- 'ctx-rh-temporalpole',
- 'ctx-rh-transversetemporal',
- 'ctx-rh-insula']
-
-
-
-
+word_young = e.load_evoked_stc('young', epoch='word', model='word % inflected'
+       , src='vol-7', inv='vec-3-MNE-0', parc='aparc+aseg')
 
 # %%
-e.sessions
+#plot.GlassBrain.butterfly(word_young['srcm'].mean('case'), cmap='Reds')
 
 # %%
-sessions = ['word']
-e.epochs
+word_old = e.load_evoked_stc('old', epoch='word', model='word % inflected'
+       , src='vol-7', inv='vec-3-MNE-0', parc='aparc+aseg')
 
 # %%
-epochs=['word-real','word-stem','word-inflected']
-group='young'
-#group='small'
-
-# %%
-e.show_subjects()
-
-# %%
-
-results={}
-for epoch in epochs:
-    for session in sessions:
-        e.set(raw='n-ica', epoch=epoch, rej='')
-        
-        result=e.load_evoked_stc(
-            subjects=group, 
-            baseline=False,
-            cov='emptyroom', 
-            model='word',
-           )
-        results[f'{epoch}_{session}'] = result
- 
-
-
-# %%
+word_aphasia = e.load_evoked_stc('aphasia', epoch='word', model='word % inflected'
+       , src='vol-7', inv='vec-3-MNE-0', parc='aparc+aseg')
 
 # %%
